093c3fe1f517852c7e2b2563150aacd65a7e3c5a
Aaron Hodgen <ahodgen@kill-9.org>
2016-02-12 09:28:23 -0500

    New asset data. Fix a tag name.

--- Qualys/Asset.hs
+++ Qualys/Asset.hs
@@ -59,11 +59,14 @@ data SimpleTag = SimpleTag
 data Tag = Tag
     { tId       :: Int
     , tName     :: Maybe Text
+    , tParentId :: Maybe Int
     , tCreated  :: Maybe UTCTime
     , tModified :: Maybe UTCTime
     , tColor    :: Maybe Text
     , tRuleText :: Maybe Text
     , tRuleType :: Maybe Text
+    , tAssetGrpId :: Maybe Int
+    , tBusUnitId  :: Maybe Int
     , tChildren :: Maybe [SimpleTag]
     } deriving (Show,Eq)
 
@@ -74,22 +77,26 @@ parseTag :: (MonadIO m, MonadThrow m) => ConduitM Event o m (Maybe Tag)
 parseTag = tagNoAttr "Tag" $ Tag
     <$> requireWith parseUInt (tagNoAttr "id" content)
     <*> tagNoAttr "name" content
+    <*> optionalWith parseUInt (tagNoAttr "parentTagId" content)
     <*> optionalWith parseDate (tagNoAttr "created" content)
     <*> optionalWith parseDate (tagNoAttr "modified" content)
     <*> tagNoAttr "color" content
     <*> tagNoAttr "ruleText" content
     <*> tagNoAttr "ruleType" content
+    <*> optionalWith parseUInt (tagNoAttr "srcAssetGroupId" content)
+    <*> optionalWith parseUInt (tagNoAttr "srcBusinessUnitId" content)
     <*> (join <$> tagNoAttr "children" (tagNoAttr "list" parseChildren))
 
 parseChildren :: (MonadIO m, MonadThrow m) => ConduitM Event o m [SimpleTag]
 parseChildren = many parseChild
 
 parseChild :: (MonadIO m, MonadThrow m) => ConduitM Event o m (Maybe SimpleTag)
-parseChild = tagNoAttr "SimpleTag" $ SimpleTag
+parseChild = tagNoAttr "TagSimple" $ SimpleTag
     <$> requireWith parseUInt (tagNoAttr "id" content)
     <*> tagNoAttr "name" content
 
-runSearchTags :: Maybe V3Options -> QualysT IO (Maybe [Tag])
+runSearchTags :: (MonadIO m, MonadThrow m) => Maybe V3Options ->
+                 QualysT m (Maybe [Tag])
 runSearchTags opt = do
     res <- processV3PageWith V3v20 "search/am/tag" opt parseTags
     return $ snd res
